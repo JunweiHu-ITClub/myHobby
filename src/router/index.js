0085c72e71e15619126955f2bab3cf012b119597
wyg <934601290@qq.com>
2018-01-08 16:53:19 +0800

    基础项目

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import answer from '@/components/answer'
 import vuexName from '@/components/vuex'
 
@@ -10,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'answer',
+      component: answer
     },
     {
       path: '/answer',
